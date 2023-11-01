d3ba3a76c89ccee4e57c4d5288ddad38dbe061b0
gabcisterna <126285779+gabcisterna@users.noreply.github.com>
2023-11-01 20:34:30 -0300

    Update ProductManager.js

--- ProductManager.js
+++ ProductManager.js
@@ -23,35 +23,42 @@ class ProductManager{
         fs.writeFileSync(this.path, data)
     }
 
-    addProduct(title, description, price, thumbnail, code, stock){
-        // Valida que los datos no esten incompletos
-        if (!title || !description || isNaN(price) || !thumbnail || !code  || isNaN(stock)) {
-            console.log("Los datos del producto son inválidos o están incompletos.")
-            return
+    addProduct(title, description, price, thumbnail, code, stock) {
+        // Valida que los datos no estén incompletos
+        if (!title || !description || isNaN(price) || !thumbnail || !code || isNaN(stock)) {
+            console.log("Los datos del producto son inválidos o están incompletos.");
+            return;
         }
-
-        // Valida el code que no se repita
-        if (code && this.products.some(product => product.code === code)) {
-            console.log(`El código ${code} ya está en uso. Elige un código único.`)
-            return
+    
+        // Asegura que el nextId sea único
+        while (this.products.some(product => product.id === this.nextId)) {
+            this.nextId++;
         }
-
+    
+        // Valida el código para que no se repita
+        if (this.products.some(product => product.code === code)) {
+            console.log(`El código ${code} ya está en uso. Elige un código único.`);
+            return;
+        }
+    
         // Carga los datos del producto
         let nuevoEvento = {
             id: this.nextId,
             title,          // nombre del producto
             description,    // descripción del producto
-            price,          // percio
+            price,          // precio
             thumbnail,      // ruta de imagen
             code,           // código identificador
-            stock           //número de piezas disponibles
-        }
-
-        // Agrega al array product un producto nuevo
-        this.products.push(nuevoEvento)
-        this.nextId++ // Incrementar el id para el próximo producto
-        this.saveProducts() // Guarda los productos en el archivo
+            stock           // número de piezas disponibles
+        };
+    
+        // Agrega al array de productos un producto nuevo
+        this.products.push(nuevoEvento);
+        this.nextId++; // Incrementar el ID para el próximo producto
+        this.saveProducts(); // Guarda los productos en el archivo
     }
+    
+    
 
     getProductById(id){
         let indice = this.products.findIndex(products => products.id === id)
@@ -73,8 +80,14 @@ class ProductManager{
             console.log(`Producto no encontrado con ID: ${id}`);
             return;
         }
-    
+
         this.products.splice(productIndex, 1);
+
+        // Actualizamos los IDs de los productos restantes
+        for (let i = productIndex; i < this.products.length; i++) {
+            this.products[i].id--;
+        }
+
         this.saveProducts(); // Guarda los cambios en el archivo
     }
 
